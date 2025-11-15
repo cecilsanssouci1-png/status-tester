2668ed7f60c67fb71420c8bae5c63b8edbd020ca
cecilsanssouci1-png <cecilsanssouci1@gmail.com>
2025-11-15 14:25:21 +0200

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-fetch('https://tfp.is')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('content').innerHTML = html;
-  })
-  .catch(err => console.error('Blocked by CORS', err));
